6f948fe87563e3e4ba20c2b69ed96a95f06a23e2
seyoon <se981106@naver.com>
2025-01-10 16:22:32 +0900

    Feat: 상품상세 수정,삭제 service,controller 추가

--- controllers/manager/product.controller.js
+++ controllers/manager/product.controller.js
@@ -1,8 +1,11 @@
 const { Category } = require('../../schemas/category/category.schema');
+
 const {
   createProduct,
   findAllProduct,
   findProductById,
+  updateProductById,
+  deleteProductById,
 } = require('../../service/product/product.service');
 /* 상품 등록 */
 const addProduct = async (req, res) => {
@@ -71,10 +74,10 @@ const getAllProduct = async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: '상품을 찾을 수 없습니다.' });
     }
-    console.log(
-      'product userId ',
-      product.map((item) => item.userId),
-    );
+    // console.log(
+    //   'product userId ',
+    //   product.map((item) => item.userId),
+    // );
     res.status(201).json({
       message: '모든상품 조회완료',
       product: product.map((item) => ({
@@ -122,8 +125,81 @@ const getProduct = async (req, res) => {
     return res.status(500).json({ message: err.message });
   }
 };
+/* 단일 상품 수정 */
+const updateProduct = async (req, res) => {
+  try {
+    const {
+      _id,
+      userId,
+      name,
+      price,
+      introduce,
+      image,
+      detailImage,
+      categoryIndex,
+    } = req.body;
+    console.log('update object ', req.body);
+
+    if (
+      !_id ||
+      !userId ||
+      !name ||
+      !price ||
+      !introduce ||
+      !image ||
+      !detailImage.length ||
+      categoryIndex === undefined ||
+      categoryIndex === null
+    ) {
+      return res
+        .status(400)
+        .json({ message: '입력이 안된 필드값이 있습니다.' });
+    }
+
+    const categories = await Category.find();
+
+    /* 상품등록에서 선택한 카테고리 index를 DB에 저장된 첫번째 카테고리 name 배열 index에 넣기 */
+    const resultCategoryName = categories[0].name[categoryIndex];
+    // console.log('categories ', categories);
+    // console.log('resultCategoryName ', resultCategoryName);
+    if (!resultCategoryName) {
+      return res
+        .status(400)
+        .json({ message: '유효하지 않은 카테고리 인덱스입니다.' });
+    }
+    const resultCategory = { name: resultCategoryName };
+
+    const updateResult = await updateProductById({
+      _id,
+      userId,
+      name,
+      price,
+      introduce,
+      image,
+      detailImage,
+      category: resultCategory,
+    });
+    // console.log('updateResult ', updateResult);
+    res.status(204).json({ message: '상품 수정 완료되었습니다.' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
+/* 단일 상품 삭제 */
+const deleteProduct = async (req, res) => {
+  try {
+    const deleted = await deleteProductById(req.params.id);
+    res.status(204).json({ message: '상품 삭제 완료되었습니다.' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
 module.exports = {
   addProduct,
   getAllProduct,
   getProduct,
+  updateProduct,
+  deleteProduct,
 };
--- service/product/product.service.js
+++ service/product/product.service.js
@@ -7,7 +7,7 @@ const createProduct = async (Data) => {
     console.log(product);
   } catch (err) {
     console.log('[createProduct] Error ', err);
-    throw new Error('상품 생성에 실패했습니다.');
+    throw new Error('상품 생성에 실패했습니다.', err);
   }
 };
 /* 모든 상품 조회 */
@@ -20,7 +20,7 @@ const findAllProduct = async () => {
     return product;
   } catch (err) {
     console.log('[getAllfindAllProduct] Error ', err);
-    throw new Error('모든 상품 조회에 실패했습니다.');
+    throw new Error('모든 상품 조회에 실패했습니다.', err);
   }
 };
 /* 단일 상품 조회 */
@@ -30,11 +30,46 @@ const findProductById = async (id) => {
     return product;
   } catch (err) {
     console.log('[getFindProductById] Error ', err);
-    throw new Error('상품 상세 조회에 실패했습니다.');
+    throw new Error('상품 상세 조회에 실패했습니다.', err);
   }
 };
+/* 단일 상품 수정 */
+const updateProductById = async ({
+  _id,
+  userId,
+  name,
+  price,
+  introduce,
+  image,
+  detailImage,
+  category,
+}) => {
+  try {
+    const update = await Product.updateOne(
+      { _id },
+      { userId, name, price, introduce, image, detailImage, category },
+    );
+    // console.log('update ', update);
+    return update;
+  } catch (err) {
+    throw new Error('단일 상품 수정에 실패했습니다.', err);
+  }
+};
+
+/* 단일 상품 삭제 */
+const deleteProductById = async (id) => {
+  try {
+    const deleted = await Product.findByIdAndDelete(id);
+    return deleted;
+  } catch (err) {
+    throw new Error('단일 상품 삭제에 실패했습니다.', err);
+  }
+};
+
 module.exports = {
   createProduct,
   findAllProduct,
   findProductById,
+  updateProductById,
+  deleteProductById,
 };
