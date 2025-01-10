d352d7aee9b7df22bbdf79ad0e42ae220f6b6cc5
yunzzng <leejiyun0114@naver.com>
2025-01-10 14:18:51 +0900

    리뷰 조회 작업 중

--- service/review/review.service.js
+++ service/review/review.service.js
@@ -7,11 +7,24 @@ const createReview = async (reviewData) => {
 };
 
 // 리뷰 조회(상세페이지)
-const findReviewsByProduct = async (productId) => {
-  return await Review.find({ productId }).populate(
-    'userId',
-    'name profileImage',
-  );
+const findReviewsByProduct = async (productId, page, itemsPerPage) => {
+  try {
+    const limit = itemsPerPage; 
+    const skip = (page - 1) * limit; 
+
+    const reviews = await Review.find({ productId })
+      .populate('userId', 'name profileImage')
+      .sort({ createdAt: -1 }) 
+      .skip(skip)
+      .limit(limit);
+
+    const totalCount = await Review.countDocuments({ productId });
+
+    return { reviews, totalCount };
+  } catch (error) {
+    console.error("[ReadReviewsByProduct] Error:", error);
+    throw new Error("리뷰 조회에 실패했습니다.", { cause: error });
+  }
 };
 
 module.exports = {
