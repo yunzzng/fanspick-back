0b464cf4e20c2a4fe64e6f25cdc318baf9b0f001
백아현 <baeg-ahyeon@baeg-ahyeon-ui-MacBookAir.local>
2025-01-10 23:25:44 +0900

    product.service,controller,route수정

--- routes/manager/manager.routes.js
+++ routes/manager/manager.routes.js
@@ -6,6 +6,7 @@ const {
   getProduct,
   updateProduct,
   deleteProduct,
+  getProductsByIds,
 } = require('../../controllers/manager/product.controller');
 const {
   getAllCategory,
@@ -16,6 +17,7 @@ const router = express.Router();
 router.post('/product', addProduct); // /api/manager/product
 router.get('/get-all-product', getAllProduct); // /api/manager/get-all-product
 router.get('/product/:id', getProduct); // /api/manager/product/:id
+router.get('/product-by-ids', getProductsByIds); // /api/manager/product-by-ids
 router.get('/category', getAllCategory); // /api/manager/category
 router.put('/update', updateProduct); // /api/manager/update
 router.delete('/delete/:id', deleteProduct); // /api/manager/delete/:id
--- service/product/product.service.js
+++ service/product/product.service.js
@@ -66,10 +66,27 @@ const deleteProductById = async (id) => {
   }
 };
 
+/*장바구니, 즐겨찾기 조회*/
+const findByIds = async (ids) => {
+  try {
+    const productDetail = await Product.find({ _id: { $in: ids } });
+
+    if (!productDetail) {
+      throw new Error('상품이 없습니다.');
+    }
+
+    return productDetail;
+  } catch (err) {
+    console.error('[findByIds] Error:', err);
+    throw new Error('상품 조회에 실패했습니다.', err);
+  }
+};
+
 module.exports = {
   createProduct,
   findAllProduct,
   findProductById,
   updateProductById,
   deleteProductById,
+  findByIds,
 };
