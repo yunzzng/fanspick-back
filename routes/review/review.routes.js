ec2c81b25097eacfaef8510acd630deb6c899934
yunzzng <leejiyun0114@naver.com>
2025-01-10 11:30:50 +0900

    리뷰 수정 작업 중

--- routes/review/review.routes.js
+++ routes/review/review.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const passport = require("passport");
-const { addReview } = require("../../controllers/review/review.controller");
+const { addReview, getReviewsByProduct } = require("../../controllers/review/review.controller");
 const router = express.Router();
 
 // 리뷰 등록
 router.post("/add", passport.authenticate("jwt", { session: false }), addReview); // /api/review/add
+
+// 리뷰 조회(상세피이지)
+router.get("/product/:productId", passport.authenticate("jwt", { session: false }), getReviewsByProduct);
 
 module.exports = router;
--- service/review/review.service.js
+++ service/review/review.service.js
@@ -1,10 +1,20 @@
-const Review = require("../../schemas/review/review.schema");
+const Review = require('../../schemas/review/review.schema');
 
+// 리뷰 등록
 const createReview = async (reviewData) => {
   const newReview = new Review(reviewData);
   return await newReview.save();
 };
 
+// 리뷰 조회(상세페이지)
+const findReviewsByProduct = async (productId) => {
+  return await Review.find({ productId }).populate(
+    'userId',
+    'name profileImage',
+  );
+};
+
 module.exports = {
   createReview,
+  findReviewsByProduct,
 };
