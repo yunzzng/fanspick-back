db0c20d51b12b387f848516431d717955ddf1066
이태희 <taehee3992@naver.com>
2025-01-12 23:09:01 +0900

    Feat: 결제 백엔드 완료

--- schemas/purchase/payment.schema.js
+++ schemas/purchase/payment.schema.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const paymentSchema = new mongoose.Schema(
@@ -6,25 +6,34 @@ const paymentSchema = new mongoose.Schema(
     userId: {
       type: ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
     },
-    /*  */
-    imp_uid: {
-      // 아임포트에서 제공하는 결제 고유 ID
+    pg: {
       type: String,
       required: true,
     },
-
+    pay_method: {
+      type: String,
+      required: true,
+    },
     /* 필수x */
     merchant_uid: {
       // 아임포트에서 제공하는 가맹점 주문 ID
       type: String,
       required: true,
     },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-module.exports = mongoose.model("Payment", paymentSchema);
+module.exports = mongoose.model('Payment', paymentSchema);
