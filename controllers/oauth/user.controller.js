c5ac54a9d58777bc3694e01c02194bc9debacb6c
yunzzng <leejiyun0114@naver.com>
2025-01-13 10:10:22 +0900

    유저 컨트롤러 수정

--- controllers/oauth/user.controller.js
+++ controllers/oauth/user.controller.js
@@ -57,7 +57,7 @@ const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: "1m" }
+      { expiresIn: "1h" }
     );
 
     const decoded = jwt.decode(token);
