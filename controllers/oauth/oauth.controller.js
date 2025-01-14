38db1c982cad53cc1f428bbf6bfa346b7ae93b63
yunzzng <leejiyun0114@naver.com>
2025-01-14 23:10:02 +0900

    oauth DB 저장

--- controllers/oauth/oauth.controller.js
+++ controllers/oauth/oauth.controller.js
@@ -1,70 +1,77 @@
 const jwt = require('jsonwebtoken');
+const {
+  createUser,
+  findUserByEmail,
+} = require('../../service/oauth/oauth.service');
 const { FRONT_URL } = require('../../consts/app');
 
 const handleSocialLoginCallback = async (req, res) => {
   const { provider, profile } = req.user;
-  console.log(`${provider} 로그인 후 req.user:`, req.user);
 
   const accessToken = req.user ? req.user.accessToken : null;
 
   if (!accessToken) {
-    console.log('Access Token 없음');
     return res.status(401).json({ message: 'Access Token 저장 실패' });
   }
 
   try {
-    const baseUser = { provider, id: profile.id, role: 'user' };
-    console.log('네이버 프로필 데이터:', req.user.profile._json);
+    const baseUser = {
+      provider,
+      role: 'user',
+      termsAccepted: true,
+    };
 
-    const user =
+    const userData =
       provider === 'kakao'
         ? {
             ...baseUser,
             name: profile._json.properties.nickname || '',
             email: '',
-            avatar: profile._json.properties.profile_image || '',
+            profileImage: profile._json.properties.profile_image || '',
           }
         : provider === 'google'
         ? {
             ...baseUser,
             name: profile.displayName || '',
             email: profile.emails?.[0]?.value || '',
-            avatar: profile.photos?.[0]?.value || '',
+            profileImage: profile.photos?.[0]?.value || '',
           }
         : provider === 'naver'
         ? {
             ...baseUser,
             name: profile._json.nickname || '',
             email: profile._json.email || '',
-            avatar: profile._json.profile_image || '',
+            profileImage: profile._json.profile_image || '',
           }
         : baseUser;
 
+    const existingUser =
+      (await findUserByEmail(userData.email)) || (await createUser(userData));
+
     const token = jwt.sign(
       {
-        id: user.id,
-        name: user.name || user.nickname || '',
-        email: user.email || '',
-        role: user.role,
+        id: existingUser._id,
+        name: existingUser.name,
+        email: existingUser.email,
+        role: existingUser.role,
       },
       process.env.JWT_SECRET,
       { expiresIn: '1h' },
     );
 
-    res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/;`);
-    console.log('JWT 토큰 생성 및 저장 완료:', token);
 
-    // res.status(200).json({
-    //   message: '로그인 성공!',
-    //   user,
-    //   token,
-    // });
+    const redirectUrl = `${FRONT_URL}oauth/callback?token=${token}&id=${
+      existingUser._id
+    }&name=${encodeURIComponent(existingUser.name)}&email=${encodeURIComponent(
+      existingUser.email,
+    )}&role=${existingUser.role}&profileImage=${encodeURIComponent(
+      existingUser.profileImage,
+    )}&provider=${provider}`; 
 
-    const redirectUrl = `${FRONT_URL}oauth/callback?token=${token}&name=${encodeURIComponent(user.name)}&email=${encodeURIComponent(user.email)}&id=${user.id}&provider=${provider}`;
-    console.log('Redirecting to:', redirectUrl);
     res.redirect(redirectUrl);
+    console.log('Redirect URL:', redirectUrl);
   } catch (error) {
-    console.error('프로필 정보 저장 중 오류 발생:', error);
+    console.error('Error occurred:', error);
     res.status(500).json({ success: false, message: '서버 오류 발생' });
   }
 };
