5d7a731fd5ad8917827d7634afb2c16c1a7e6a68
백아현 <baeg-ahyeon@baeg-ahyeon-ui-MacBookAir.local>
2025-01-15 17:52:55 +0900

    에러처리  수정

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500; // 상태 코드가 없으면 500
+  const status = err.status || 500; // 상태 코드가 없으면 500//
   const message = err.message || '서버 오류가 발생했습니다.';
 
   console.error(`[Error] ${status} - ${message} - ${req.method} ${req.url}`);
